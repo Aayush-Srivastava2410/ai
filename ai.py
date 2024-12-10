adfc72e0298127437d8654b9123d4ab55dea7e2f
Slide-bit <7g02.aayush@gmail.com>
2024-12-10 22:48:41 +0530

    t2s yay

--- ai.py
+++ ai.py
@@ -1,6 +1,9 @@
 from dspbot import token
 from random import choice
 import google.generativeai as genai
+import t2s as tts
+
+
 queryText='How does AI work'
 wordLimit=['50', '100', '150', '200']
 
@@ -8,5 +11,7 @@ genai.configure(api_key=token)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 
-response = model.generate_content(queryText+"in {0} words".format(choice(wordLimit)))
-print(response.text)
+response = model.generate_content(queryText+"in about 50 words")
+tts.T2S(
+    response.text
+).save('1.mp3')
